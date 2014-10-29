3022fda8e93d2fc0828b6ccc73db9383e670266d
Tyler Waldron <tylerwal@gmail.com>
2014-10-28 22:09:43 -0400

    Initial Arduino Server

--- ArduinoFun/Program.cs
+++ ArduinoFun/Program.cs
@@ -1,95 +1,36 @@
 ﻿using System;
 using System.IO.Ports;
-using System.Threading;
 
 namespace ArduinoFun
 {
-	class Program
+	public class Program
 	{
-		private static SerialPort _serialPort;
-
-		private static bool _continue;
-
-		public static void Main()
+		public static void Main(string[] args)
 		{
-			StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
-			//Thread readThread = new Thread(Read);
+			Program prog = new Program();
+			prog.ReadArduino();
+		}
 
-			// Create a new SerialPort object with default settings.
-			_serialPort = new SerialPort
+		private void ReadArduino()
+		{
+			SerialPort serialPort = new SerialPort("COM3", 9600)
 			{
-				BaudRate = 9600, // 9600 default
-//				DataBits = 8, // 8 default
-//				DiscardNull = false, // false default
-				DtrEnable = true, // false default
-//				Handshake = Handshake.None, // None default
-//				Parity = Parity.None, // None default
-//				ParityReplace = 63,
-				PortName = "COM3",  // COM1 default
-//				ReadBufferSize = 4096, // 4096 default
-//				ReadTimeout = -1, 
-//				ReceivedBytesThreshold = 1,
-				RtsEnable = true,
-//				StopBits = StopBits.One,
-//				WriteBufferSize = 2048,
-//				WriteTimeout = -1
+				DtrEnable = true
 			};
 
-			_serialPort.DataReceived += _serialPort_DataReceived;
+			serialPort.Open();
 
-			// Allow the user to set the appropriate properties.
-			// Set the read/write timeouts
+			serialPort.DataReceived += SerialPortDataReceived;
 
-			_serialPort.Open();
-			_continue = true;
-			//readThread.Start();
-
-			//Console.Write("Name: ");
-			//string name = Console.ReadLine();
-
-			//Console.WriteLine("Type QUIT to exit");
-
-			//_serialPort.Write(Environment.NewLine);
-
-			while (_continue)
+			while (true)
 			{
-//				string message = Console.ReadLine();
-//
-//				if (stringComparer.Equals("quit", message))
-//				{
-//					_continue = false;
-//				}
-//				else
-//				{
-//					_serialPort.WriteLine(
-//					    String.Format("<{0}>: {1}", name, message)
-//					    );
-//				}
 			}
-
-//			readThread.Join();
-			_serialPort.Close();
 		}
 
-		static void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+		private void SerialPortDataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
 		{
-			SerialPort sp = (SerialPort)sender;
-			string indata = sp.ReadExisting();
-			//Console.WriteLine("Data Received:");
-			Console.WriteLine(indata);
-		}
-
-		public static void Read()
-		{
-			while (_continue)
-			{
-				try
-				{
-					string message = _serialPort.ReadLine();
-					Console.WriteLine(message);
-				}
-				catch (TimeoutException) { }
-			}
+			string line = ((SerialPort)sender).ReadLine();
+			Console.WriteLine(line);
 		}
 	}
 }
---
+++ TempServer/TempServer.ino
@@ -0,0 +1,150 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <string.h>
+#include "DHT.h"
+
+DHT dht;
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+EthernetServer server(80); // Port 80 is http.
+
+#define bufferMax 128
+int bufferSize;
+char buffer[bufferMax];
+
+char cmd[15];    // increase these if you need longer commands/parameters
+char param1[15];
+char param2[15];
+
+void setup()
+{
+ Ethernet.begin(mac, ip);
+ server.begin();
+
+ Serial.begin(9600);
+ Serial.print("server is at ");
+ Serial.println(Ethernet.localIP());
+  
+ dht.setup(2); // data pin 2
+}
+
+void loop()
+{
+ EthernetClient client = server.available();
+ if (client)
+ {
+   WaitForRequest(client);
+   ParseReceivedRequest();
+   PerformRequestedCommands();
+   
+   client.stop();
+ }
+}
+
+void WaitForRequest(EthernetClient client) // Sets buffer[] and bufferSize
+{
+ bufferSize = 0;
+
+ while (client.connected()) {
+   if (client.available()) {
+     char c = client.read();
+     if (c == '\n')
+       break;
+     else
+       if (bufferSize < bufferMax)
+         buffer[bufferSize++] = c;
+       else
+         break;
+   }
+ }
+ 
+ PrintNumber("bufferSize", bufferSize);
+}
+
+void ParseReceivedRequest()
+{
+ Serial.println("in ParseReceivedRequest");
+ Serial.println(buffer);
+ 
+ //Received buffer contains "GET /cmd/param1/param2 HTTP/1.1".  Break it up.
+ char* slash1;
+ char* slash2;
+ char* slash3;
+ char* space2;
+ 
+ slash1 = strstr(buffer, "/") + 1; // Look for first slash
+ slash2 = strstr(slash1, "/") + 1; // second slash
+ slash3 = strstr(slash2, "/") + 1; // third slash
+ space2 = strstr(slash2, " ") + 1; // space after second slash (in case there is no third slash)
+ if (slash3 > space2) slash3=slash2;
+
+ PrintString("slash1",slash1);
+ PrintString("slash2",slash2);
+ PrintString("slash3",slash3);
+ PrintString("space2",space2);
+ 
+ // strncpy does not automatically add terminating zero, but strncat does! So start with blank string and concatenate.
+ cmd[0] = 0;
+ param1[0] = 0;
+ param2[0] = 0;
+ strncat(cmd, slash1, slash2-slash1-1);
+ strncat(param1, slash2, slash3-slash2-1);
+ strncat(param2, slash3, space2-slash3-1);
+ 
+ PrintString("cmd",cmd);
+ PrintString("param1",param1);
+ PrintString("param2",param2);
+}
+
+void PerformRequestedCommands()
+{
+ if ( strcmp(cmd,"getTemp") == 0 ) GetTemp();
+}
+
+void GetTemp()
+{
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+  
+  if (strcmp(param1, "temp") == 0) 
+  {
+    server.print(dht.toFahrenheit(temperature), 1);
+  }
+  else if (strcmp(param1, "humidity") == 0) 
+  {
+    server.print(humidity, 1);    
+  }
+  else if (strcmp(param1, "status") == 0) 
+  {
+    server.print(dht.getStatusString());   
+  }
+  else
+  {
+    server.print("Status: ");
+    server.print(dht.getStatusString());
+    server.println("");
+    server.print("Humidity: ");
+    server.print(humidity, 1);
+    server.println("");
+    server.print("Temperature: ");
+    server.println(dht.toFahrenheit(temperature), 1);
+  }
+}
+
+void PrintString(char* label, char* str)
+{
+ Serial.print(label);
+ Serial.print("=");
+ Serial.println(str);
+}
+
+void PrintNumber(char* label, int number)
+{
+ Serial.print(label);
+ Serial.print("=");
+ Serial.println(number, DEC);
+}
